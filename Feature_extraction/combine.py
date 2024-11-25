c3612dbf46b79ca1b9c34c8471b1b08ac3a3c0b7
Njq0104 <2961810830@qq.com>
2024-11-25 20:03:45 +0800

    Update combine.py

--- Feature_extraction/combine.py
+++ Feature_extraction/combine.py
@@ -1,13 +1,9 @@
 
 import numpy as np
-pos_data = np.load("/home/ys/nijiaqi/features/Prot-t5_xl_uniref50/t5_data_pos_780.npy")
-neg_data = np.load("/home/ys/nijiaqi/features/Prot-t5_xl_uniref50/t5_data_neg_780.npy")
+pos_data = np.load("feature.npy")
+neg_data = np.load("feature.npy")
 
 combined_data = np.vstack((pos_data, neg_data))
 
-np.save("/home/ys/nijiaqi/Convolutional-KANs-master/features/Prot-t5-merged.npy", combined_data)
+np.save("feature.npy", combined_data)
 
-
-print(pos_data.shape)
-print(neg_data.shape)
-print("combined data shape:", combined_data.shape)
